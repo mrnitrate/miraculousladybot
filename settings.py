76763f2e2e32332ae7858545dd9f30dcb7be51d2
aemil estvold <mrnitrate@gmail.com>
2016-06-30 17:49:06 -0600

    update tumblr oauth keys

--- settings.py
+++ settings.py
@@ -2,10 +2,10 @@ import pytumblr
 import psycopg2
 
 client = pytumblr.TumblrRestClient( #https://api.tumblr.com/console
-    '<consumer_key>',
-    '<consumer_secret>',
-    '<oauth_token>',
-    '<oauth_secret>',
+    '<EBA7cZNiWUKwuw7waQ12rf2YBUwmlHWh45VwgGhdjVXTwpWdJH>',
+    '<PIFMt7nyx3bHoQY8ixbqEgrnx4orIfqNyhy5kWLRgbWrYg0iNt>',
+    '<OC6bmFeYDHj61iqYZ0ZDWcFLI5EjCWbiICzTjEegBDoRh3UqaY>',
+    '<LM27nLuW2BqiHBBgFSeznJ10koxaMoon1uYi1TI0KATdrUJMOD>',
 )
 
 conn = psycopg2.connect("dbname=fanfictions user=postgres")
